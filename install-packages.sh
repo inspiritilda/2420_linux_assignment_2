9e7d46f552bf3af71b98dec07502f3f3d0ec6b7e
Matilda <matilda8202@gmail.com>
2024-11-06 19:44:00 +0000

    install-packages.sh edit

--- install-packages.sh
+++ install-packages.sh
@@ -3,22 +3,18 @@
 # This script will install the required software packages on the system.
 
 # Define a list of packages to install
-PACKAGES=("kakoune" "tmux")
+PACKAGES=("kakoune" "tmux") # An array named PACKAGES which contains two elements: kakoune (text editor) and tmux (terminal multiplexer)
 
 # Function to install packages
 install_packages() {
     echo "Updating system package list..."
-    # Install packages listed in PACKAGES array
-    sudo pacman -Syu --noconfirm "${PACKAGES[@]}"
+    sudo pacman -Syu --noconfirm "${PACKAGES[@]}" # Install packages listed in the array above
 
     # Check if installation was successful
-    if [[ $? -ne 0 ]]; then
+    if [[ $? -ne 0 ]]; then # If the last command failed, execute the code block that follows
         echo "Something went wrong. The packages didn't get installed."
-        exit 1
+        exit 1 # Indicated failure or an error
     else
         echo "All packages installed successfully."
     fi
 }
-
-# To run the install function
-install_packages
